5f161eb189a3f0949fc6145cda129011c24026d1
Leonid Rozenberg <leonidr@gmail.com>
2016-05-07 20:17:02 -0400

    Refine testing to include all alignment files.

--- src/scripts/test_parsing.ml
+++ src/scripts/test_parsing.ml
@@ -1,47 +1,25 @@
 (* ./test.native ../foreign/IMGTHLA/ *)
 open Printf 
+module String = Sosa.Native_string
 
 let to_suffix = function
-  | `gen  -> "gen"
-  | `nuc  -> "nuc"
-  | `prot -> "prot"
+  | `gen  -> "gen.txt"
+  | `nuc  -> "nuc.txt"
+  | `prot -> "prot.txt"
 
-let to_fnames ?(suffix=`gen) dir =
-  let ss = to_suffix suffix in
-  List.map (fun s -> Filename.concat dir (sprintf "%s_%s.txt" s ss)) 
-    [ "alignments/A"
-    ; "alignments/B"
-    ; "alignments/C"
-    ; "alignments/DMA"
-    ; "alignments/DMB"
-    ; "alignments/DOA"
-    ; "alignments/DOB"
-    ; "alignments/DPA1"
-    ; "alignments/DPB1"
-    ; "alignments/DPB2"
-    ; "alignments/DQA1"
-    ; "alignments/DQB1"
-    ; "alignments/DRA"
-    ; "alignments/DRB1"
-    ; "alignments/DRB3"
-    ; "alignments/DRB4"
-    ; "alignments/E"
-    ; "alignments/F"
-    ; "alignments/G"
-    ; "alignments/HFE"
-    ; "alignments/H"
-    ; "alignments/J"
-    ; "alignments/K"
-    ; "alignments/L"
-    ; "alignments/MICA"
-    ; "alignments/MICB"
-    ; "alignments/P"
-    ; "alignments/TAP1"
-    ; "alignments/TAP2"
-    ; "alignments/V"
-    ; "alignments/Y"
-    ] 
-
+let to_fnames ?suffix dir =
+  let filter =
+      match suffix with
+      | None ->
+        fun l -> l
+      | Some t ->
+        let suffix = to_suffix t in
+        List.filter (String.is_suffix ~suffix)
+  in
+  Sys.readdir dir
+  |> Array.to_list
+  |> filter
+  |> List.map (Filename.concat dir)
 
 let () =
   if !Sys.interactive || Array.length Sys.argv = 1 then
@@ -49,5 +27,9 @@ let () =
   else
     to_fnames Sys.argv.(1)
     |> List.iter (fun f ->
+        try
         let _p = Mas_parser.from_file f in
-        Printf.printf "parsed %s\n" f)
+        Printf.printf "parsed %s\n" f
+        with e ->
+          Printf.eprintf "failed to parse %s\n" f;
+          raise e)
