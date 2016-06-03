df0fd8321748da96ffacb419bca721bd56cd3be3
Leonid Rozenberg <leonidr@gmail.com>
2016-06-03 19:12:35 -0400

    Implement K-mer folding

--- src/lib/ref_graph.ml
+++ src/lib/ref_graph.ml
@@ -53,7 +53,7 @@ end
 
 module G = Imperative.Digraph.ConcreteLabeled(Nodes)(Edges)
 
-(** Traversing **)
+(** Traversal **)
 exception Found of Nodes.t
 
 let next_node_along allele g ~from =
@@ -84,23 +84,93 @@ let fold_along_allele ~start allele g ~f ~init =
   in
   loop start (f init start)
 
-(* If this was a GADT we could encode the p s v inside the
-   N that we're matching! *)
-let vsplit_at g allele ~pos p s v =
+module Tg = Topological.Make_stable (G)
+
+(*
+let over_whole_kmers ~l ~k s ~f ~init =
+  let e = l - k in
+  let rec loop a i =
+    if i > e then a else loop (f a (String.sub_exn s i k)) (i + 1)
+  in
+  loop init 0
+
+let suffixes ~l ~k s =
+  List.init (k - 1) ~f:(fun i ->
+    let j = i + 1 in
+    j, String.sub_exn s (l - j) j)
+
+let individual ~l ~k s =
+  List.init l ~f:(fun i ->
+      (k - l + i, String.sub_exn s i (l - i)))
+   *)
+
+(* Fold over whole k-mers in s, but return an assoc of length remaining
+   and suffixes that are not matched. *)
+let only_over_whole_kmers ?(canonical=false) k s f init =
+  let l = String.length s in
+  let rec loop i (a, lst) =
+    if i = l then (a, lst) else
+      let sub_length = l - i in
+      if sub_length >= k then
+        loop (i + 1) (f a (String.sub_exn s i k), lst)
+      else
+        loop (i + 1) (a, (k - sub_length, String.sub_exn s i sub_length)::lst)
+  in
+  loop 0 (init, [])
+  |> (fun (s, l) -> s, if canonical then List.rev l else l)
+
+let fold_kmers ?(canonical=false) k g f init =
   let open Nodes in
-  let index = pos - p in
-  let fs, sn = String.split_at s ~index in
-  let pr = G.pred_e g v in
-  let su = G.succ_e g v in
-  G.remove_vertex g v;
-  let v1 = N (p, fs) in
-  G.add_vertex g v1;
-  List.iter ~f:(fun (p, l, _) -> G.add_edge_e g (G.E.create p l v1)) pr;
-  let v2 = N (pos, sn) in
-  G.add_vertex g v2;
-  G.add_edge_e g (G.E.create v1 allele v2);
-  List.iter ~f:(fun (_, l, s) -> G.add_edge_e g (G.E.create v2 l s)) su;
-  (v1, v2)
+  let rec over_succ state node = function
+    | [] -> state
+    | k_seq_lst ->
+        (* Since we're folding over each edge, we'll get the successor node 'v'
+          for every allele edge. *)
+        G.fold_succ (fun node ((state, set) as st) ->
+            if List.mem node ~set then st else
+              (* This logic makes it DFS *)
+              let nstate = proc_succ state k_seq_lst node in
+              nstate, node :: set) g node (state, [])
+        |> fst
+  and proc_succ state k_seq_lst node =
+    match node with
+      | S _       -> invalid_argf "Start should not be a successor!"
+      | E _       -> state
+      | B _       -> over_succ state node k_seq_lst
+      | N (_, s)  ->
+          let l = String.length s in
+          List.fold_left k_seq_lst ~init:(state,[])
+            ~f:(fun (state, acc) (k, seq) ->
+                  if k <= l then
+                    let ns = String.concat [seq; String.sub_exn s 0 k] in
+                    let nstate = f state ns in
+                    nstate, acc
+                  else
+                    state, (k - l, String.concat [seq; s]) :: acc)
+          |> fun (state, ksl) ->
+                over_succ state node (if canonical then List.rev ksl else ksl)
+  in
+  let proc node state =
+    match node with
+    | S _ | E _ | B _ -> state
+    | N (_, s)        ->
+      let nstate, lst = only_over_whole_kmers ~canonical k s f state in
+      over_succ nstate node lst
+  in
+  Tg.fold proc g init
+
+let between g start stop =
+  let ng = G.create () in
+  G.add_vertex ng start;
+  let rec add_from node =
+    G.iter_succ_e (fun ((_, _, n) as e) ->
+      G.add_vertex ng n;
+      G.add_edge_e ng e;
+      if n <> stop then add_from n)
+      g node
+  in
+  add_from start;
+  ng
 
 (** Output **)
 
@@ -126,6 +196,8 @@ let output_dot ?short fname g =
   in
   Dot.output_graph oc g;
   close_out oc
+
+
 
 let output ?(pdf=true) ?(open_=true) ~short fname g =
   output_dot ~short (fname ^ ".dot") g;
