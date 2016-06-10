dbeeee14225ded21c2427376fc404fafd852ce61
Leonid Rozenberg <leonidr@gmail.com>
2016-06-10 11:52:33 -0400

    Fix references in script

--- src/scripts/just_parse.ml
+++ src/scripts/just_parse.ml
@@ -4,5 +4,5 @@ let d = "../foreign/IMGTHLA/alignments" ;;
 let mp = Mas_parser.from_file (Filename.concat d f) ;;
 let just_gaps = (function Mas_parser.Gap _ -> true | _ -> false) ;;
 let just_bns = (function Mas_parser.Boundary _ -> true | _ -> false) ;;
-let oa = "C*01:02:02";;
-List.assoc oa mp.Mas_parser.alt_elems |> List.map Mas_parser.al_el_to_string |> List.iter (Printf.printf "%s\n") ;;
+let oa = "A*26:40";;
+List.assoc oa mp.Mas_parser.alt_elems |> List.map ~f:Mas_parser.al_el_to_string |> List.iter ~f:(Printf.printf "%s\n") ;;
