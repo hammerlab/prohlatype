b8c20193f636b43f44566e32a0ac77bc69ee286b
Leonid Rozenberg <leonidr@gmail.com>
2016-07-07 11:52:10 -0400

    Change script to export print_parsed function

--- src/scripts/just_parse.ml
+++ src/scripts/just_parse.ml
@@ -4,5 +4,7 @@ let d = "../foreign/IMGTHLA/alignments" ;;
 let mp = Mas_parser.from_file (Filename.concat d f) ;;
 let just_gaps = (function Mas_parser.Gap _ -> true | _ -> false) ;;
 let just_bns = (function Mas_parser.Boundary _ -> true | _ -> false) ;;
-let oa = "A*26:40";;
-List.assoc oa mp.Mas_parser.alt_elems |> List.map ~f:Mas_parser.al_el_to_string |> List.iter ~f:(Printf.printf "%s\n") ;;
+let print_parsed oa =
+  List.assoc oa mp.Mas_parser.alt_elems
+  |> List.map ~f:Mas_parser.al_el_to_string
+  |> List.iter ~f:(Printf.printf "%s\n") ;;
