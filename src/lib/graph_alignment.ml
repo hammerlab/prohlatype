35913e2eae88bd9adc6880c2e530934ae530cddf
Leonid Rozenberg <leonidr@gmail.com>
2016-07-13 01:43:58 -0400

    Keep only what we need

--- src/lib/graph_alignment.ml
+++ src/lib/graph_alignment.ml
@@ -66,7 +66,7 @@ module Ms = Set.Make (
   end)
 
 let debug_ref = ref false
-let fail_ref = ref true
+let fail_ref = ref false
 
 let compute_mismatches ?(search_pos_start=0) {g; aindex; bounds} search_seq pos =
   let open Nodes in
@@ -76,13 +76,8 @@ let compute_mismatches ?(search_pos_start=0) {g; aindex; bounds} search_seq pos 
   let nmas = num_mismatches_against_seq search_seq in
   let nmasb = num_mismatches_against_seq_backwards search_seq in
   (* Return the edges that we have seen -> we have to backfill those we haven't seen! *)
-  let assign_mismatches_for_node ?starting_with mismatches node =
-    let init =
-      match starting_with with
-      | Some s -> s
-      | None   -> Alleles.Set.empty ()
-    in
-    (*G.fold_succ_e (fun (_, e, _) ea ->*)
+  let assign_mismatches_for_node mismatches node =
+    let init = Alleles.Set.empty () in
     G.fold_succ_e (fun (_, e, _) ea ->
       if mismatches <> 0 then begin
         if !debug_ref then
@@ -159,7 +154,15 @@ let compute_mismatches ?(search_pos_start=0) {g; aindex; bounds} search_seq pos 
         let after_align edge_opt mismatches st f =
           match edge_opt with
           | Some edge ->
-              assign_mismatches_to_edge mismatches edge;
+              (*assign_mismatches_to_edge mismatches edge; *)
+              G.iter_pred_e (fun (_, e, _) ->
+                if mismatches <> 0 then begin
+                  if !debug_ref then
+                    printf "Asgn_msm NODE Edge adding %d mismatches to: %s\n"
+                      mismatches (Alleles.Set.to_string aindex e);
+                  Alleles.Map.update_from e mis_map ((+) mismatches)
+                end)
+                g node;
               f st
           | None      ->
               let seen_edges = assign_mismatches_for_node mismatches node in
@@ -245,73 +248,6 @@ let compute_mismatches ?(search_pos_start=0) {g; aindex; bounds} search_seq pos 
   in
   mis_map
 
-(*
-type 'e pa =
-  { mismatches : int
-  ; edge       : 'e             (*BitSet.t        too big to carry around? *)
-  ; alignment  : 'e alignment
-  }
-and 'e alignment = Leaf of int
-                 | Partial of 'e pa list
-
-let rec map_alignment f = function
-  | Leaf n     -> Leaf n
-  | Partial pl -> Partial (List.map pl ~f:(fun p ->
-      { p with edge = f p.edge ; alignment = map_alignment f p.alignment }))
-
-(* Add max number of mismatches as argument *)
-let thread_through_graph ~mub g search_seq search_pos_start pos =
-  let open Nodes in
-  let open Graph_index in
-  let align_against_seq = align_against_seq search_seq in
-  let rec descend node m0 ~search_pos =
-    G.fold_succ_e (fun (_, edge, vs) acc ->
-        match vs with
-        | S _             -> invalid_argf "Another start?"
-        | E _             -> acc
-        | B _             -> (* This is a weird case since all prev nodes point at a boundary
-                                the acc in this case should be [] *)
-                              assert (acc = []);
-                              descend vs m0 ~search_pos
-        | N (_, node_seq) ->
-            match align_against_seq ~search_pos ~node_seq ~node_offset:0 with
-            | `Finished mismatches            ->
-                let tm = m0 + mismatches in
-                if tm > mub then acc else
-                  { mismatches ; edge; alignment = Leaf tm } :: acc
-            | `GoOn (mismatches, search_pos)  ->
-                let tm = m0 + mismatches in
-                if tm > mub then acc else
-                  let alignment = Partial (descend vs tm ~search_pos) in
-                  { mismatches ; edge; alignment } :: acc)
-      g node []
-  in
-  let { sequence; offset; _ } = pos in
-  match align_against_seq ~search_pos:search_pos_start ~node_seq:sequence ~node_offset:offset with
-  | `Finished mismatches           ->
-      if mismatches > mub then None else
-        Some (Leaf mismatches)
-  | `GoOn (mismatches, search_pos) ->
-      if mismatches > mub then None else
-        let node = N (pos.alignment, pos.sequence) in
-        Some (Partial (descend node mismatches ~search_pos))
-
-let align ?(mub=max_int) g index search_seq =
-  let open Graph_index in
-  let km1 = k index - 1 in
-  starting_with index search_seq >>= (fun lst ->
-      Ok (List.filter_map lst ~f:(thread_through_graph ~mub g search_seq km1)))
-
-let name_edges_in_alignment aindex =
-  map_alignment (Alleles.Set.to_human_readable aindex)
-
-let rec best_of_paths = function
-  | Leaf n      -> n
-  | Partial pl  ->
-      let bps = List.map ~f:(fun pa -> best_of_paths pa.alignment) pl in
-      List.fold_left ~f:min ~init:max_int bps
-      (* if pl = [] then there are no alignments that satisfy the mub *)
-
 let to_weights lst =
   let flst = List.map ~f:float_of_int lst in
   let ilst = List.map ~f:(fun x -> 1. /. (1. +. x)) flst in
@@ -322,20 +258,9 @@ let init_alignment_map aindex =
   Alleles.Map.make aindex 0.
 
 (* Weighing Alignments ... inference *)
-let alignments_to_weights ?(base_weight=1.) amap al =
-  let rec descend w0 = function
-  | Leaf _     -> ()
-  | Partial pl -> let weights = to_weights (List.map pl ~f:(fun pa -> pa.mismatches)) in
-                  List.iter2 pl weights ~f:(fun pa w ->
-                    let w_i = w *. w0 in
-                    Alleles.Map.update_from pa.edge amap ((+.) w_i);
-                    descend w_i pa.alignment)
-  in
-  descend base_weight al
 
 let most_likely aindex amap =
   Alleles.Map.fold aindex ~init:[] ~f:(fun acc v allele ->
     if v > 0. then (v,allele) :: acc else acc) amap
   |> List.sort ~cmp:(fun ((v1 : float), _) (v2,_) -> compare v2 v1)
 
-*)
