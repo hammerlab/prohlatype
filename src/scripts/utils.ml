b8bfdbc136c01a30ea47b1b12e01741027462d5d
Leonid Rozenberg <leonidr@gmail.com>
2016-07-21 17:57:45 -0400

    Moved the code to ref_graph

--- src/scripts/utils.ml
+++ src/scripts/utils.ml
@@ -8,10 +8,3 @@ let e_n_lst_to_out aindex lst =
       (Nodes.vertex_name n)
       (Alleles.Set.to_human_readable ~compress:true aindex e))
 
-let nodesset_to_string s =
-  let open Ref_graph in
-  NodesSet.fold ~f:(fun n a -> Nodes.vertex_name n :: a) s ~init:[]
-  |> String.concat ~sep:"; "
-  |> sprintf "{%s}"
-
-
