32a7294acd9439fd055826d88c55612748561437
Leonid Rozenberg <leonidr@gmail.com>
2016-07-27 13:19:41 -0400

    Clean up some warts

--- src/scripts/same_alignments_test.ml
+++ src/scripts/same_alignments_test.ml
@@ -24,7 +24,8 @@ let reads_from_fastq file =
     !li
 
 let reads =
-  reads_from_fastq "/Users/leonidrozenberg/Documents/projects/hlatyping/upenn/opti/merged/120013_TGACCA/120013_TGACCA_2fin.fastq"
+  reads_from_fastq
+    "/Users/leonidrozenberg/Documents/projects/hlatyping/upenn/opti/merged/120013_TGACCA/120013_TGACCA_2fin.fastq"
 
 let greads =
   List.filter reads ~f:(fun r -> match String.index_of_character r 'N' with | Some _ -> false | _ -> true)
@@ -32,11 +33,23 @@ let greads =
 
 let al_to_list idx r = Alleles.Map.fold idx ~f:(fun acc c s -> (s, c) :: acc ) ~init:[]  r |> List.sort ~cmp:compare
 
-let test_case ?k ~gi ~length ~read =
+let test_case ?compare_pos ?k ~gi ~length read =
   let _, (g, idx) = g_and_idx ?k gi in
   let sub_read = String.sub_exn ~index:0 ~length read in
-  let pos = Index.lookup idx sub_read |> unwrap_ok |> List.hd_exn in
-  (*let search_pos_start = (Option.value ~default:10 k) - 1 in *)
+  let pos =
+    let open Index in
+    match lookup idx sub_read with
+    | Error m     -> invalid_argf "error looking up %s in index: %s" sub_read m
+    | Ok []       -> invalid_argf "empty position returned looking up %s in index" sub_read
+    | Ok (h :: t) ->
+          match compare_pos with
+          | None   -> h
+          | Some p ->
+              let dp = p.alignment + p.offset in
+              match List.find (h :: t) ~f:(fun pn -> pn.alignment + pn.offset = dp) with
+              | None -> invalid_argf "Couldn't find desired pos %d in second graph index! for sub_read: %s" dp sub_read
+              | Some p -> p
+  in
   let al = Alignment.compute_mismatches g sub_read pos |> unwrap_ok in
   let lal = al_to_list g.Ref_graph.aindex al in
   g, idx, pos, sub_read, (List.rev lal)
@@ -46,9 +59,9 @@ let reads_with_kmers ?k ~gi =
   Array.to_list greads
   |> List.filter_map ~f:(fun s -> Index.lookup idx s |> unwrap_ok |> function | [] -> None | _ -> Some s)
 
-let just_lal ?k ~gi ~length read =
-  let _g, _idx, _pos, _sub_read, lal = test_case ?k ~gi ~length ~read in
-  lal
+let just_lal ?k ?compare_pos ~gi ~length read =
+  let _g, _idx, pos, _sub_read, lal = test_case ?compare_pos ?k ~gi ~length read in
+  pos, lal
 
 let find_bad ?(length=100) ?(k=10) ?(stop=3000) start_size =
   let start_reads = reads_with_kmers ~k ~gi:start_size in
@@ -57,16 +70,17 @@ let find_bad ?(length=100) ?(k=10) ?(stop=3000) start_size =
       start_reads
   in
   let diff_lals new_size prev_lals =
-    List.fold_left prev_lals ~init:([], []) ~f:(fun (nacc, wacc) (read, prev_lal) ->
-      let lal_new = just_lal ~k ~length ~gi:new_size read in
-      let diff_opt =
-        List.fold_left prev_lal ~init:[] ~f:(fun acc (a, c) ->
-          let new_c = List.assoc a lal_new in
-          if new_c <> c then (a, c) :: acc else acc)
-      in
-      match diff_opt with
-      | [] -> ((read, lal_new) :: nacc), wacc
-      | dl -> ((read, lal_new) :: nacc), (read, dl) :: wacc)
+    List.fold_left prev_lals ~init:([], [])
+      ~f:(fun (nacc, wacc) (read, (compare_pos, prev_lal)) ->
+            let (pos_new, lal_new) = just_lal ~compare_pos ~k ~length ~gi:new_size read in
+            let diff_opt =
+              List.fold_left prev_lal ~init:[] ~f:(fun acc (a, c) ->
+                let new_c = List.assoc a lal_new in
+                if new_c <> c then (a, c) :: acc else acc)
+            in
+            match diff_opt with
+            | [] -> ((read, (pos_new, lal_new)) :: nacc), wacc
+            | dl -> ((read, (pos_new, lal_new)) :: nacc), (read, dl) :: wacc)
   in
   let rec loop prev_size old_lals =
     let gi = prev_size + 1 in
@@ -79,12 +93,22 @@ let find_bad ?(length=100) ?(k=10) ?(stop=3000) start_size =
   in
   loop start_size start_lals
 
+let describe_error ?(length=20) read gi =
+  let gim1 = gi - 1 in
+  let cur = !Alignment.debug_ref in
+  Alignment.debug_ref := true;
+  let gnm1, i_nm1, pnm1, snm1, alnm1 = test_case ~k:10 ~gi:gim1 ~length read in
+  let gn, i_n, pn, sn, aln = test_case ~compare_pos:pnm1 ~k:10 ~gi ~length read in
+  Alignment.debug_ref := cur;
+  (gnm1, i_nm1, pnm1, snm1, alnm1), (gn, i_n, pn, sn, aln)
+
 
 let () =
   if !Sys.interactive then () else
     let n = Array.length Sys.argv in
     let length = if n <= 1 then 20 else int_of_string Sys.argv.(1) in
-    let bad_size, bad_elems = find_bad ~length 2 in
-    printf "found bad alignments %d with graph size: %d\n"
-      (List.length bad_elems) bad_size
+    let start = if n <= 2 then 2 else int_of_string Sys.argv.(2) in
+    let bad_size, bad_elems = find_bad ~length start in
+    printf "found bad alignments %d with graph size: %d and read length: %d\n"
+      (List.length bad_elems) bad_size length
 
