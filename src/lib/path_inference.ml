2b02e356d88f9ef439835dc8878b6b2386235f00
Leonid Rozenberg <leonidr@gmail.com>
2016-09-23 14:39:59 -0400

    Implement a filter for constraining which sequences we compute likelihoods

--- src/lib/path_inference.ml
+++ src/lib/path_inference.ml
@@ -3,56 +3,62 @@
 
 open Util
 
-let likelihood ?(alph_size=4) ?(er=0.01) ~len mismatches =
+let log_likelihood ?(alph_size=4) ?(er=0.01) ~len mismatches =
   let lmp = log (er /. (float (alph_size - 1))) in
   let lcp = log (1. -. er) in
-  let c = len - mismatches in
-  exp ((float c) *. lcp +. (float mismatches) *. lmp)
+  let c = (float len) -. mismatches in
+  c *. lcp +. mismatches *. lmp
+
+let likelihood ?alph_size ?er ~len m =
+  exp (log_likelihood ?alph_size ?er ~len m)
 
 let report_mismatches aindex mm =
-  Alleles.Map.values_assoc aindex mm
-  |> List.sort ~cmp:(fun (i1,_) (i2,_) -> compare i1 i2)
-  |> List.iter ~f:(fun (w, a) ->
-      printf "%02d\t%s\n" w
-        (insert_chars ['\t'; '\t'; '\n']
-          (Alleles.Set.to_human_readable aindex ~max_length:1000 ~complement:`No a)))
+  printf "reporting mismatches\n";
+  let () =
+    Alleles.Map.values_assoc aindex mm
+    |> List.sort ~cmp:(fun (i1,_) (i2,_) -> compare i1 i2)
+    |> List.iter ~f:(fun (w, a) ->
+        printf "%02d\t%s\n" w
+          (insert_chars ['\t'; '\t'; '\n']
+            (Alleles.Set.to_human_readable aindex ~max_length:10000 ~complement:`No a)))
+  in
+  printf "finished reporting mismatches\n%!"
 
-let one ?(verbose=false) ?(multi_pos=`Best) ?(as_likelihood=true) g idx seq =
+let one ?(verbose=false) ?(multi_pos=`Best) g idx seq =
   let report mm =
     if verbose then report_mismatches g.Ref_graph.aindex mm else ()
   in
-  let len = String.length seq in
-  let to_output m = if as_likelihood then likelihood ~len m else float m in
-  let to_output_mm mm = Alleles.Map.map_wa mm ~f:to_output in
+  (*let len = String.length seq in *)
+  let to_float_map = Alleles.Map.map_wa ~f:float in
   Index.lookup idx seq >>= function
     | []      -> error "no index found!"
     | h :: t  ->
       Alignment.compute_mismatches g seq h >>= fun mm ->
         report mm;
         match t with
-        | [] -> Ok (to_output_mm mm)
+        | [] -> Ok (to_float_map mm)
         | tl ->
             match multi_pos with
-            | `TakeFirst        -> Ok (to_output_mm mm)
+            | `TakeFirst        -> Ok (to_float_map mm)
             | `Average          ->
                 let n = 1 + List.length t in
                 let weight = 1. /. (float n) in
-                let lm = Alleles.Map.map_wa (to_output_mm mm) ~f:(fun m -> m /. weight) in
+                let lm = Alleles.Map.map_wa (to_float_map mm) ~f:(fun m -> m /. weight) in
                 let rec loop = function
                   | []     -> Ok lm
                   | p :: t ->
                       Alignment.compute_mismatches g seq p >>= fun mm ->
                           report mm;
-                          Alleles.Map.update2 mm lm (fun m c -> c +. weight *. (to_output m));
+                          Alleles.Map.update2 mm lm (fun m c -> c +. weight *. (float m));
                           loop t
                 in
                 loop tl
             (* Find the best alignment, the one with the lowest number of
-               mismatches, over all positions! *)
+               mismatches, over all indexed positions! *)
             | `Best             ->
                 let current_best = Alleles.Map.fold_wa mm ~init:max_int ~f:min in
                 let rec loop b bm = function
-                  | []     -> Ok (to_output_mm bm)
+                  | []     -> Ok (to_float_map bm)
                   | p :: t ->
                       (* TODO: If this becomes a bottleneck, we can stop [compute_mismatches]
                          early if the min mismatch > b *)
@@ -66,19 +72,55 @@ let one ?(verbose=false) ?(multi_pos=`Best) ?(as_likelihood=true) g idx seq =
                 in
                 loop current_best mm tl
 
-let multiple_fold ?verbose ?multi_pos ?as_likelihood g idx =
-  let init = Alleles.Map.make g.Ref_graph.aindex 0. in
+let mx b amap =
+  let n, s = Alleles.Map.fold_wa ~f:(fun (n,s) v -> (n+1, s+.v)) ~init:(0,0.) amap in
+  (float n) /. s < b
+
+let yes _ = true
+
+let multiple_fold_lst ?verbose ?multi_pos ?filter g idx =
+  let list_map = Alleles.Map.make g.Ref_graph.aindex [] in
+  let filter = match filter with | None -> yes | Some n -> mx n in
+  let update v l = v :: l in
   let fold amap seq =
-    one ?verbose ?multi_pos ?as_likelihood g idx seq >>= fun m ->
-      Alleles.Map.update2 m amap (+.); Ok amap
+    one ?verbose ?multi_pos g idx seq >>= fun m ->
+      if filter m then begin
+        Alleles.Map.update2 ~source:m ~dest:amap update;
+        match verbose with | Some true -> printf "passed filter\n" | _ -> ()
+      end else begin
+        match verbose with | Some true -> printf "did not pass filter\n" | _ -> ()
+      end;
+      Ok amap
+  in
+  list_map, fold
+
+let multiple_fold ?verbose ?multi_pos ?as_ ?filter g idx =
+  let zero_map = Alleles.Map.make g.Ref_graph.aindex 0. in
+  let one_map = Alleles.Map.make g.Ref_graph.aindex 1. in
+  let filter = match filter with | None -> yes | Some n -> mx n in
+  let init, update =
+    match as_ with
+    | None
+    | Some `Mismatches    -> zero_map, (+.)
+    | Some `LogLikelihood -> zero_map, (fun s m -> s +. log_likelihood ~len:100 m) (* TODO: dep on seq *)
+    | Some `Likelihood    -> one_map,  (fun s m -> s *. likelihood ~len:100 m)     (* TODO: dep on seq *)
+  in
+  let fold amap seq =
+    one ?verbose ?multi_pos g idx seq >>= fun m ->
+      if filter m then begin
+        Alleles.Map.update2 ~source:m ~dest:amap update;
+        match verbose with | Some true -> printf "passed filter\n" | _ -> ()
+      end else begin
+        match verbose with | Some true -> printf "did not pass filter\n" | _ -> ()
+      end;
+      Ok amap
   in
   init, fold
 
-let multiple ?multi_pos ?as_likelihood g idx =
+let multiple ?multi_pos ?as_ g idx =
   let init, f = multiple_fold g idx in
   let rec loop a = function
     | []     -> Ok a
     | h :: t -> f a h >>= fun m -> loop m t
   in
   loop init
-
