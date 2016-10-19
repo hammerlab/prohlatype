9aca2752b6d86f3edcfe80c7ed7babadb8809b8b
Leonid Rozenberg <leonidr@gmail.com>
2016-10-18 23:24:34 -0400

    Adapt the indexing to encode across N

--- src/lib/index.ml
+++ src/lib/index.ml
@@ -92,6 +92,7 @@ let fold_over_kmers_in_graph ~k ~f ~init ~extend ~close g =
   in
   Tg.fold proc g.g init
 
+(* Not 'N' tolerant. *)
 let kmer_counts ~k g =
   let init = Kmer_table.make k 0 in
   let f tbl (_al, sequence) { index; length = `Whole } =
@@ -127,34 +128,24 @@ let specific_position g allele pos =
 (* A Graph index *)
 type t = position list Kmer_table.t
 
+(* The failed method were we store the penultimate position of the Kmer *)
 let create_at_penultimate ~k g =
   let init = Kmer_table.make k [] in
   let f tbl (alignment, sequence) { index; length = `Whole } =
-    let i = Kmer_to_int.encode sequence ~pos:index ~len:k in
+    let ilst = Kmer_to_int.encode_N_tolerant sequence ~pos:index ~len:k in
     let p = { alignment; sequence; offset = index + k - 1 } in
-    (*let () = printf "Adding %d \t %s \t %d\n" alignment (Ref_graph.index_string sequence p.offset) p.offset in *)
-    Kmer_table.update (fun lst -> p :: lst) tbl i;
+    List.iter ilst ~f:(Kmer_table.update (fun lst -> p :: lst) tbl);
     tbl
   in
   let extend (al, sequence) { index; length = `Part len } ext_opt =
-    (*let () = printf "At %d, %s extend at %d len %d" al (Ref_graph.index_string sequence index) index len in *)
     match ext_opt with
-    | None     ->
-        let nj = Kmer_to_int.encode sequence ~pos:index ~len in
-        (*let () = printf " cur: None \t nj %d \n" nj in *)
-        nj
-    | Some ext ->
-        let nj = Kmer_to_int.encode sequence ~pos:index ~len ~ext in
-        (*let () = printf " cur: Some %d \t nj %d\n" ext nj in *)
-        nj
+    | None      -> Kmer_to_int.encode_N_tolerant ~pos:index ~len sequence
+    | Some exts -> Kmer_to_int.encode_N_tolerant sequence ~pos:index ~len ~exts
   in
-  let close tbl (alignment, sequence) { index; length = `Part len} ext =
-    let i = Kmer_to_int.encode sequence ~pos:index ~len ~ext in
+  let close tbl (alignment, sequence) { index; length = `Part len} exts =
+    let ilst = Kmer_to_int.encode_N_tolerant sequence ~pos:index ~len ~exts in
     let p = { alignment; sequence; offset = index + len - 1 } in
-    (*let () = printf "Closing %d \t %s \t %d\t ext: %d \n"
-      alignment (Ref_graph.index_string sequence p.offset) p.offset ext
-    in *)
-    Kmer_table.update (fun lst -> p :: lst) tbl i;
+    List.iter ilst ~f:(Kmer_table.update (fun lst -> p :: lst) tbl);
     tbl
   in
   fold_over_kmers_in_graph ~k g ~f ~close ~extend ~init
@@ -162,10 +153,9 @@ let create_at_penultimate ~k g =
 let create ~k g =
   let init = Kmer_table.make k [] in
   let f tbl (alignment, sequence) { index; length = `Whole } =
-    let i = Kmer_to_int.encode sequence ~pos:index ~len:k in
+    let ilst = Kmer_to_int.encode_N_tolerant sequence ~pos:index ~len:k in
     let p = { alignment; sequence; offset = index } in
-    (*let () = printf "Adding %d \t %s \t %d\n" alignment (Ref_graph.index_string sequence p.offset) p.offset in *)
-    Kmer_table.update (fun lst -> p :: lst) tbl i;
+    List.iter ilst ~f:(Kmer_table.update (fun lst -> p :: lst) tbl);
     tbl
   in
   let extend (alignment, sequence) { index; length = `Part len } ext_opt =
@@ -173,21 +163,16 @@ let create ~k g =
           (Ref_graph.index_string sequence index) index len in *)
     match ext_opt with
     | None     ->
-        let nj = Kmer_to_int.encode sequence ~pos:index ~len in
+        let nj = Kmer_to_int.encode_N_tolerant sequence ~pos:index ~len in
         let p  = { alignment; sequence; offset = index } in
-        (*let () = printf " cur: None \t nj %d \n" nj in *)
         (nj, p)
-    | Some (ext, p) ->
-        let nj = Kmer_to_int.encode sequence ~pos:index ~len ~ext in
-        (*let () = printf " cur: Some %d \t nj %d\n" ext nj in *)
+    | Some (exts, p) ->
+        let nj = Kmer_to_int.encode_N_tolerant sequence ~pos:index ~len ~exts in
         nj, p
   in
-  let close tbl (_al, sequence) { index; length = `Part len} (ext, p) =
-    let i = Kmer_to_int.encode sequence ~pos:index ~len ~ext in
-    (*let () = printf "Closing %d \t %s \t %d\t ext: %d \n"
-      alignment (Ref_graph.index_string sequence p.offset) p.offset ext
-    in *)
-    Kmer_table.update (fun lst -> p :: lst) tbl i;
+  let close tbl (_al, sequence) { index; length = `Part len} (exts, p) =
+    let ilst = Kmer_to_int.encode_N_tolerant sequence ~pos:index ~len ~exts in
+    List.iter ilst ~f:(Kmer_table.update (fun lst -> p :: lst) tbl);
     tbl
   in
   fold_over_kmers_in_graph ~k g ~f ~close ~extend ~init
