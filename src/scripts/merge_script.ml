d641fc474312f393546ed79452620f1e298b9fa6
Leonid Rozenberg <leonidr@gmail.com>
2016-10-25 14:24:04 -0400

    Add fully working merge script

--- src/scripts/merge_script.ml
+++ src/scripts/merge_script.ml
@@ -18,6 +18,9 @@ let matches_boundary { index; position } = function
   | Mas_parser.Boundary b when b.idx = index &&  b.pos = position -> true
   | _ -> false
 
+let bm_to_string { index; position } =
+  sprintf "{index: %d; position: %d}" index position
+
 let bm_to_boundary_string { index; position } =
   sprintf "{idx: %d; pos: %d}" index position
 
@@ -45,10 +48,21 @@ type instruction =
     { nuclear   : boundary_marker
     ; genetic   : boundary_marker
     ; sequence  : string
-    ; offset    : int
     }
 
-(* Nuc goes into gen! *)
+let instruction_to_string = function
+  | FillFromGenetic { genetic; sequence } ->
+      sprintf "FillFromGenetic { genentic: %s; sequence: %s}"
+        (bm_to_string genetic) (short_seq sequence)
+  | MergeFromNuclear { nuclear; genetic; sequence } ->
+      sprintf "MergeFromNuclear { nuclear: %s; genetic: %s; sequence: %s }"
+        (bm_to_string nuclear) (bm_to_string genetic) (short_seq sequence)
+
+let bounded_results_to_string lst =
+  String.concat ~sep:";"
+    (List.map lst ~f:(fun (bm, seq) ->
+      sprintf "(%s, %s)" (bm_to_string bm) (short_seq seq)))
+
 let zip_align ~gen ~nuc =
   let rec loop acc g n =
     match n with
@@ -61,11 +75,11 @@ let zip_align ~gen ~nuc =
     | (nuclear, nuc_sequence) :: nt  ->
         begin
           match g with
-          | []       -> Error n
+          | []       -> error "Reached end of genetic sequence before nuclear: %s"
+                          (bounded_results_to_string n)
           | (genetic, sequence) :: gt ->
               if sequence = nuc_sequence then
-                let offset = genetic.position - nuclear.position in
-                let instr = MergeFromNuclear { nuclear; genetic; sequence; offset } in
+                let instr = MergeFromNuclear { nuclear; genetic; sequence } in
                 loop (instr :: acc) gt nt
               else
                 let instr = FillFromGenetic { genetic; sequence } in
@@ -203,11 +217,18 @@ let drop_until_boundary ~bm lst =
 
 let first_position =
   let open Mas_parser in function
-    | Start s     -> s
-    | Gap g       -> g.start
-    | Boundary b  -> b.pos
-    | v           -> invalid_argf "Can't start with %s" (al_el_to_string v)
+    | Start s     -> Ok s
+    | Gap g       -> Ok g.start
+    | Boundary b  -> Ok b.pos
+    | v           -> error "Can't start with %s" (al_el_to_string v)
 
+let alignment_elements_to_string lst =
+  String.concat ~sep:";" (List.map ~f:Mas_parser.al_el_to_string lst)
+
+(* Nuc goes into gen!
+  - What about if the underlying merging sequences do not have the sequences
+    across the boundaries assumed by the instructions generated against the
+    reference? *)
 let apply_align_instr ~gen ~nuc start_pos alg =
   let rec start gen nuc = function
     (* Assume that the instructions start with a fill command from genetic sequences *)
@@ -217,7 +238,9 @@ let apply_align_instr ~gen ~nuc start_pos alg =
             ~bm:`Nothing
         in
         loop state ~gen ~nuc tl
-    | _ -> assert false
+    | []     -> error "Empty instructions"
+    | s :: _ -> error "Did not start with a starting FillFromGenetic but %s"
+                  (instruction_to_string s)
   and loop state ~gen ~nuc = function
     | MergeFromNuclear m :: tl ->
         let bm =
@@ -239,32 +262,105 @@ let apply_align_instr ~gen ~nuc start_pos alg =
         in
         loop nstate ~gen ~nuc tl
     | [] ->
-        (*
         if gen <> [] then
-          invalid_argf "Gen not empty: %s."
-            (String.concat ~sep:";" (List.map ~f:Mas_parser.al_el_to_string gen));
-        if nuc <> [] then
-          invalid_argf "Nuc not empty: %s."
-            (String.concat ~sep:";" (List.map ~f:Mas_parser.al_el_to_string nuc)); *)
-        List.rev state.acc, gen, nuc
+          error "After all instructions genetic not empty: %s."
+            (alignment_elements_to_string gen)
+        else if nuc <> [] then
+          error "After all instructions nuclear sequence not empty: %s."
+            (alignment_elements_to_string nuc)
+        else
+          Ok (List.rev state.acc)
   in
   start gen nuc alg
 
-let reference_positions_align lst =
+let reference_positions_align ?seq lst =
   let open Mas_parser in
+  let emst = Option.value (Option.map seq ~f:(sprintf "For seq %s ")) ~default:"" in
+  let nerror n p = error "%spositions are not aligned: %s, pos: %d" emst (al_el_to_string n) p in
   let rec loop p = function
     | []                      -> Ok p
-    | (Boundary b as v) :: t  -> if b.pos = p then loop (p + 1) t else Error (al_el_to_string v, p)
-    | (Start s as v) :: t     -> if s = p then loop p t else Error (al_el_to_string v, p)
-    | (End e as v) :: t       -> if e = p then loop p t else Error (al_el_to_string v, p)
-    | (Gap g as v) :: t       -> if g.start = p then loop (p + g.length) t else Error (al_el_to_string v, p)
-    | (Sequence s as v) :: t  -> if s.start = p then loop (p + (String.length s.s)) t else Error (al_el_to_string v, p)
+    | (Boundary b as v) :: t  -> if b.pos = p then loop (p + 1) t else nerror v p
+    | (Start s as v) :: t     -> if s = p then loop p t else nerror v p
+    | (End e as v) :: t       -> if e = p then loop p t else nerror v p
+    | (Gap g as v) :: t       -> if g.start = p then loop (p + g.length) t else nerror v p
+    | (Sequence s as v) :: t  -> if s.start = p then loop (p + (String.length s.s)) t else nerror v p
   and start = function
     | (Boundary b) :: t  -> loop b.pos t
     | (Start s) :: t     -> loop s t
     | (Gap g) :: t       -> loop g.start t
-    | x :: _ -> invalid_argf "Can't start with %s" (al_el_to_string x)
-    | []     -> invalid_argf "Empty"
+    | x :: _             -> error "Can't start with %s." (al_el_to_string x)
+    | []                 -> error "Empty"
   in
   start lst
 
+let list_fold_ok lst ~f ~init =
+  let rec loop acc = function
+    | []      -> Ok acc
+    | h :: t  -> f acc h >>= fun a -> loop a t
+  in
+  loop init lst
+
+let init_trie elems =
+  let open Nomenclature in
+  list_fold_ok elems ~init:Trie.empty ~f:(fun trie s ->
+    parse s >>= fun (_gene, allele_resolution) ->
+      Ok (Trie.add allele_resolution trie))
+
+module RMap = Map.Make (struct
+    type t = Nomenclature.resolution
+    let compare = compare
+    end)
+
+let init_trie_and_map elems =
+  let open Nomenclature in
+  list_fold_ok elems ~init:(Trie.empty, RMap.empty)
+    ~f:(fun (trie, mp) (s, seq) ->
+        parse s >>= fun (_gene, allele_resolution) ->
+          Ok ( Trie.add allele_resolution trie
+             , RMap.add allele_resolution seq mp))
+
+let new_alts ~gen ~nuc start_pos instr trie rmp =
+  let open Nomenclature in
+  list_fold_ok nuc ~init:[] ~f:(fun acc (alt_name, nuc) ->
+    (* Check that the gene is always the same? *)
+    parse alt_name >>= fun (gene, allele_resolution) ->
+      let closest_allele_res = Trie.nearest allele_resolution trie in
+      let gen = RMap.find closest_allele_res rmp in
+      let closest_allele_str = resolution_to_string ~gene closest_allele_res in
+      apply_align_instr ~gen ~nuc start_pos instr >>= fun new_alts ->
+        let seq = sprintf "%s -> %s" alt_name closest_allele_str in
+        reference_positions_align ~seq new_alts >>= fun _alt_seq_pos_check ->
+          Ok ((alt_name, new_alts) :: acc))
+
+(* TODO. We need to make a decision about suffixed (ex. with 'N') alleles
+   since they may be the closest. *)
+
+let merge_and_check prefix =
+  let open Mas_parser in
+  let gen_mp, nuc_mp, instr_ok = align_from_prefix prefix in
+  instr_ok >>= fun instr ->
+    if gen_mp.reference <> nuc_mp.reference then
+      error "References don't match %s vs %s" gen_mp.reference nuc_mp.reference
+    else if gen_mp.align_date <> nuc_mp.align_date then
+      error "Align dates don't match %s vs %s" gen_mp.align_date nuc_mp.align_date
+    else
+      match List.hd gen_mp.ref_elems with
+      | None             -> error "empty reference elems!"
+      | Some hd_ref_elem ->
+        first_position hd_ref_elem >>= fun start_pos ->
+          apply_align_instr ~gen:gen_mp.ref_elems ~nuc:nuc_mp.ref_elems start_pos instr >>= fun new_ref_elems ->
+            reference_positions_align ~seq:"gen_mp.reference" new_ref_elems >>= fun _ref_position_check ->
+              let seq_assoc = (gen_mp.reference, gen_mp.ref_elems) :: gen_mp.alt_elems in
+              init_trie_and_map seq_assoc >>= fun (gtrie, rmap) ->
+                new_alts gen_mp.alt_elems nuc_mp.alt_elems start_pos instr gtrie rmap >>= fun alt_lst ->
+                  Ok  { align_date = gen_mp.align_date
+                      ; reference  = gen_mp.reference
+                      ; ref_elems  = new_ref_elems
+                      ; alt_elems  = alt_lst
+                      }
+
+let merge_test () =
+  ok_prefix
+  |> list_fold_ok ~init:() ~f:(fun () p ->
+      Printf.printf "testing: %s\n" p;
+      merge_and_check p >>= fun _ -> Ok ())
