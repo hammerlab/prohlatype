1a94c098f308efd5fbcbba1f8f808601f6b3e3c3
Leonid Rozenberg <leonidr@gmail.com>
2016-11-19 15:37:19 -0500

    Fix comma as period typo

--- src/scripts/mas_align_script.sh
+++ src/scripts/mas_align_script.sh
@@ -5,6 +5,6 @@ set -e
 echo A_gen && ./mas_align_test.native A_gen
 echo A_nuc && ./mas_align_test.native A_nuc
 echo B_gen && ./mas_align_test.native B_gen
-echo B_nuc && ,/mas_align_test.native B_nuc "B*44:02:01:02S"
+echo B_nuc && ./mas_align_test.native B_nuc "B*44:02:01:02S"
 echo C_gen && ./mas_align_test.native C_gen
 echo C_nuc && ./mas_align_test.native C_nuc "C*04:09N"
