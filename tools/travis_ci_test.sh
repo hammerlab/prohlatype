c18b2112386fe657ed37d0ec7febe7a6fe0c33eb
Leonid Rozenberg <leonidr@gmail.com>
2016-12-12 14:11:31 -0500

    Pin biocaml with git

--- tools/travis_ci_test.sh
+++ tools/travis_ci_test.sh
@@ -7,8 +7,7 @@ git clone https://github.com/ANHIG/IMGTHLA.git
 eval `opam config env`
 export OPAMYES="true"
 
-opam pin add solvuu-buld https://github.com/solvuu/solvuu-build
-opam pin add biocaml https://github.com/biocaml/biocaml/
+opam pin add biocaml https://github.com/biocaml/biocaml.git
 make setup
 
 # for test coverage
