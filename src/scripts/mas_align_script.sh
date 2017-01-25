f8878b9b1ae4296dfb3e92d265c5e2acbbfe8f49
Leonid Rozenberg <leonidr@gmail.com>
2017-01-25 16:57:48 -0500

    Sigh, another diff that now we know about

--- src/scripts/mas_align_script.sh
+++ src/scripts/mas_align_script.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo A_gen && ./mas_align_test.native A_gen
-echo A_nuc && ./mas_align_test.native A_nuc
+echo A_nuc && ./mas_align_test.native A_nuc "A*01:11N"
 echo B_gen && ./mas_align_test.native B_gen
-echo B_nuc && ./mas_align_test.native B_nuc "B*44:02:01:02S"
+echo B_nuc && ./mas_align_test.native B_nuc "B*44:02:01:02S" "B*07:44N"
 echo C_gen && ./mas_align_test.native C_gen
 echo C_nuc && ./mas_align_test.native C_nuc "C*04:09N"
