22d2219b699b1fe71739420abbedcb4e707a8a9e
Leonid Rozenberg <leonidr@gmail.com>
2017-01-30 15:36:21 -0500

    Expose the distance to neighbors option for inference

--- src/lib/path_inference.ml
+++ src/lib/path_inference.ml
@@ -42,7 +42,7 @@ module type Single_config = sig
      via the [product] type. *)
   type stop_parameter
 
-  (* [compute ~early_stop g thread position] Meausre the metric for [thread]
+  (* [compute ~early_stop g thread position] Measure the metric for [thread]
      against graph [g] at [position] and optionally stopping because of
      [early_stop]. *)
   val compute : ?early_stop:stop_parameter -> Ref_graph.t -> thread ->
@@ -80,6 +80,9 @@ type sequence_alignment_error =
 
 module AgainstSequence (C : Single_config) = struct
 
+  let lookup ?distance idx thread =
+    Index.lookup ?distance idx (C.thread_to_seq thread)
+
   let against_positions ?early_stop g ps seq =
     let res = List.map ps ~f:(C.compute ?early_stop g seq) in
     let f = function | Finished f -> `Fst f | Stopped s -> `Snd s in
@@ -88,30 +91,30 @@ module AgainstSequence (C : Single_config) = struct
     | [], als     -> Error (AllComputesStopped (List.length als))
     | r :: rs, _  -> Ok (C.reduce_across_positions r rs)
 
-  let single_no_rc ?early_stop g idx seq =
-    match Index.lookup idx (C.thread_to_seq seq) with
+  let single_no_rc ?distance ?early_stop g idx seq =
+    match lookup ?distance idx seq with
     | Error e -> Error (SequenceTooShort e)
     | Ok []   -> Error NoIndexedPositions
     | Ok ps   -> against_positions ?early_stop g ps seq
 
-  let single_with_rc_with_choice ?early_stop g idx seq =
+  let single_with_rc_with_choice ?distance ?early_stop g idx seq =
     let ap st ps seq = against_positions ?early_stop g ps seq >>| fun e -> (e, st) in
-    let is = Index.lookup idx (C.thread_to_seq seq) in
+    let is = lookup ?distance idx seq in
     match is with
     | Error e -> Error (SequenceTooShort e)
       (* No point checking rev comp since it should have the same length! *)
     | Ok ls   ->
-        let rev_seq = C.reverse_complement seq in
-        begin match ls, Index.lookup idx (C.thread_to_seq rev_seq) with
+        let rev_thread = C.reverse_complement seq in
+        begin match ls, lookup ?distance idx rev_thread with
           | _, Error e ->
               failwithf "Reverse complement sequence %s shorter than original %s!"
-                (C.thread_to_seq seq) (C.thread_to_seq rev_seq)
+                (C.thread_to_seq seq) (C.thread_to_seq rev_thread)
           | [], Ok []  -> Error NoIndexedPositions
           | ps, Ok []  -> ap `Reg ps seq
-          | [], Ok rps -> ap `Comp rps rev_seq
+          | [], Ok rps -> ap `Comp rps rev_thread
           | ps, Ok rps ->
               let reg_e = ap `Reg ps seq in
-              let rcp_e = ap `Comp rps rev_seq in
+              let rcp_e = ap `Comp rps rev_thread in
               begin match reg_e, rcp_e with
                 | Error e,          Error _                    -> reg_e
                 | Ok _,             Error _                    -> reg_e
@@ -124,18 +127,18 @@ module AgainstSequence (C : Single_config) = struct
               end
         end
 
-  let single_with_rc ?early_stop g idx seq =
-    single_with_rc_with_choice ?early_stop g idx seq >>| fun (r, _) -> r
+  let single_with_rc ?distance ?early_stop g idx seq =
+    single_with_rc_with_choice ?distance ?early_stop g idx seq >>| fun (r, _) -> r
 
-  let single ?(check_rc=true) ?early_stop g idx seq =
+  let single ?(check_rc=true) ?distance ?early_stop g idx seq =
     if check_rc then
-      single_with_rc ?early_stop g idx seq
+      single_with_rc ?distance ?early_stop g idx seq
     else
-      single_no_rc ?early_stop g idx seq
+      single_no_rc ?distance ?early_stop g idx seq
 
-  let paired ?early_stop g idx seq1 seq2 =
-    single_with_rc_with_choice ?early_stop g idx seq1 >>= fun (m1, c1) ->
-      single_with_rc_with_choice ?early_stop g idx seq2 >>= fun (m2, c2) ->
+  let paired ?distance ?early_stop g idx seq1 seq2 =
+    single_with_rc_with_choice ?distance ?early_stop g idx seq1 >>= fun (m1, c1) ->
+      single_with_rc_with_choice ?distance ?early_stop g idx seq2 >>= fun (m2, c2) ->
         match c1, c2 with
         | `Reg,  `Reg   -> Error (InconsistentPairOrientation true)
         | `Comp, `Comp  -> Error (InconsistentPairOrientation false)
@@ -290,12 +293,14 @@ module type Multiple_config = sig
 
   val map :
     ?check_rc:bool ->
+    ?distance:int ->
     ?early_stop:stop_parameter ->
     Ref_graph.t -> Index.t ->
     thread ->
     (mp Alleles.Map.t, sequence_alignment_error) result
 
   val map_paired :
+    ?distance:int ->
     ?early_stop:stop_parameter ->
     Ref_graph.t -> Index.t ->
     thread ->
@@ -308,25 +313,25 @@ end (* Multiple_config *)
 
 module Multiple (C : Multiple_config) = struct
 
-  let fold_over_fastq ?number_of_reads fastq_file ?check_rc ?early_stop g idx =
+  let fold_over_fastq ?number_of_reads fastq_file ?check_rc ?distance ?early_stop g idx =
     let amap = Alleles.Map.make g.Ref_graph.aindex C.empty in
     let f (errors, amap) fqi =
       match C.to_thread fqi with
       | Error e     -> ((ToThread e, fqi) :: errors), amap
       | Ok seq  ->
-          match C.map ?check_rc ?early_stop g idx seq with
+          match C.map ?distance ?check_rc ?early_stop g idx seq with
           | Error e -> ((e, fqi) :: errors), amap
           | Ok a    -> Alleles.Map.update2 ~source:a ~dest:amap C.reduce;
                        errors, amap
     in
     Fastq.fold ?number_of_reads ~init:([], amap) ~f fastq_file
 
-  let map_over_fastq ?number_of_reads fastq_file ?check_rc ?early_stop g idx =
+  let map_over_fastq ?number_of_reads fastq_file ?check_rc ?distance ?early_stop g idx =
     let f fqi =
       match C.to_thread fqi with
       | Error e     -> Error (ToThread e, fqi)
       | Ok seq  ->
-          match C.map ?check_rc ?early_stop g idx seq with
+          match C.map ?distance ?check_rc ?early_stop g idx seq with
           | Error e -> Error (e, fqi)
           | Ok a    -> Ok a
     in
@@ -334,25 +339,18 @@ module Multiple (C : Multiple_config) = struct
       ~f:(fun acc fqi -> (f fqi) :: acc)
     |> List.rev
 
-  let fold_over_paired ?(verbose=false) ?number_of_reads file1 file2 ?early_stop g idx =
+  let fold_over_paired ?number_of_reads file1 file2 ?distance ?early_stop g idx =
     let amap = Alleles.Map.make g.Ref_graph.aindex C.empty in
     let f (errors, amap) fqi1 fqi2 =
-      if verbose then Printf.printf "typing %s"
-        fqi1.Biocaml_unix.Fastq.name;
       match C.to_thread fqi1 with
-      | Error e     -> if verbose then Printf.printf " \t no thread1\n%!";
-                       ((ToThread e, fqi1) :: errors), amap
+      | Error e     -> ((ToThread e, fqi1) :: errors), amap
       | Ok seq1     ->
           match C.to_thread fqi2 with
-          | Error e -> if verbose then Printf.printf " \t no thread2\n%!";
-                       ((ToThread e, fqi2) :: errors), amap
+          | Error e -> ((ToThread e, fqi2) :: errors), amap
           | Ok seq2 ->
-            if verbose then Printf.printf " \t mapping";
-            match C.map_paired ?early_stop g idx seq1 seq2 with
-            | Error e -> if verbose then Printf.printf " \t errored \n%!";
-                          ((e, fqi1) :: errors), amap      (* TODO: return both fqi1 and fqi2 ? *)
-            | Ok a    -> if verbose then Printf.printf " \t ok \n%!";
-                         Alleles.Map.update2 ~source:a ~dest:amap C.reduce;
+            match C.map_paired ?distance ?early_stop g idx seq1 seq2 with
+            | Error e -> ((e, fqi1) :: errors), amap      (* TODO: return both fqi1 and fqi2 ? *)
+            | Ok a    -> Alleles.Map.update2 ~source:a ~dest:amap C.reduce;
                          errors, amap
     in
     let res =
@@ -362,7 +360,7 @@ module Multiple (C : Multiple_config) = struct
     match res with
     | `DesiredReads (errors, amap)               -> errors, amap
     | `BothFinished (errors, amap)               -> errors, amap
-    (* TODO, go to single. *)
+    (* Continue typing on the single? *)
     | `OneReadPairedFinished (_, (errors, amap)) -> errors, amap
 
 end (* Multiple *)
@@ -403,12 +401,12 @@ module Llhd_config = struct
   type thread = SequenceMismatches.thread
 
   let to_thread fqi = Ok fqi.Biocaml_unix.Fastq.sequence
-  let map l ?check_rc ?early_stop g idx th =
+  let map l ?check_rc ?distance ?early_stop g idx th =
     let len = String.length th in
     SequenceMismatches.single ?check_rc ?early_stop g idx th >>= fun m ->
       Ok (Alleles.Map.map_wa ~f:(l ~len) m)
 
-  let map_paired l ?early_stop g idx th1 th2 =
+  let map_paired l ?distance ?early_stop g idx th1 th2 =
     let len = String.length th1 in
     SequenceMismatches.paired ?early_stop g idx th1 th2 >>= fun m ->
       Ok (Alleles.Map.map_wa ~f:(l ~len) m)
@@ -434,14 +432,14 @@ module Phred_lhd = Multiple (struct
     | CR.Error e -> Error (CE.to_string_hum e)
     | CR.Ok qarr -> Ok (fqi.Biocaml_unix.Fastq.sequence, qarr) *)
 
-  let map ?check_rc ?early_stop g idx th =
+  let map ?check_rc ?distance ?early_stop g idx th =
     let open Alignment in
-    PhredLlhdMismatches.single ?check_rc ?early_stop g idx th >>= fun amap ->
+    PhredLlhdMismatches.single ?distance ?check_rc ?early_stop g idx th >>= fun amap ->
       Ok (Alleles.Map.map_wa amap ~f:(fun pt -> pt.PhredLikelihood_config.sum_llhd))
 
-  let map_paired ?early_stop g idx th1 th2 =
+  let map_paired ?distance ?early_stop g idx th1 th2 =
     let open Alignment in
-    PhredLlhdMismatches.paired ?early_stop g idx th1 th2 >>= fun amap ->
+    PhredLlhdMismatches.paired ?distance ?early_stop g idx th1 th2 >>= fun amap ->
       Ok (Alleles.Map.map_wa amap ~f:(fun pt -> pt.PhredLikelihood_config.sum_llhd))
 
   let reduce = (+.)
