1435efae15cfd8e359c80d9541191c650f9bab78
Leonid Rozenberg <leonidr@gmail.com>
2017-03-04 15:40:22 -0500

    Implement backward pass

--- src/lib/baq.ml
+++ src/lib/baq.ml
@@ -13,11 +13,6 @@ let default_probabilities ?(read_length=100) () =
   ; ending        = 1. /. (2.0 *. float read_length)
   }
 
-let match_idx     = 0
-let insertion_idx = 1
-let deletion_idx  = 2
-let start_end_idx = 3 (* Compress the two nodes ala the Samtools note. *)
-
 module Float = struct
   let ( + ) x y = x +. y
   let ( - ) x y = x -. y
@@ -25,53 +20,68 @@ module Float = struct
   let ( / ) x y = x /. y
 end
 
-let to_transition_matrix ?(model_probs=`Default) read_length ref_length =
-  let mp =
-    match model_probs with
-    | `Default                 -> default_probabilities ~read_length ()
-    | `WithAverageReadLength l -> default_probabilities ~read_length:l ()
-    | `Spec model              -> model
-  in
-  (* Rename the probabilities to follow the convention in the paper.
-     Still uncertain if it actually makes it easier to understand transition
-     probability calculations. *)
-  let alpha = mp.gap_open in
-  let beta  = mp.gap_extension in
-  let gamma = mp.ending in
-  let tm = Array.make_matrix ~dimx:4 ~dimy:4 0. in
-  let ll = float ref_length in
-  let open Float in
-  tm.(match_idx).(match_idx)            <- (1. - 2. * alpha) * (1. - gamma);
-  tm.(match_idx).(insertion_idx)        <- alpha * (1. - gamma);
-  tm.(match_idx).(deletion_idx)         <- alpha * (1. - gamma);
-  tm.(match_idx).(start_end_idx)        <- gamma;
+module TransitionMatrix = struct
 
-  tm.(insertion_idx).(match_idx)        <- (1. - beta) * (1. - gamma);
-  tm.(insertion_idx).(insertion_idx)    <- beta * (1. - gamma);
-  (*tm.(insertion_idx).(deletion_idx)   <- 0.; *)
-  tm.(insertion_idx).(start_end_idx)    <- gamma;
+  let match_idx     = 0
+  let insertion_idx = 1
+  let deletion_idx  = 2
+  let start_end_idx = 3 (* Compress the two nodes ala the Samtools note. *)
 
-  tm.(deletion_idx).(match_idx)         <- (1. - beta);
-  (*tm.(deletion_idx).(insertion_idx)   <- 0.; *)
-  tm.(deletion_idx).(deletion_idx)      <- beta;
-  (*tm.(deletion_idx).(end_idx)         <- 0.; *)
+  let init ?(model_probs=`Default) read_length ref_length =
+    let mp =
+      match model_probs with
+      | `Default                 -> default_probabilities ~read_length ()
+      | `WithAverageReadLength l -> default_probabilities ~read_length:l ()
+      | `Spec model              -> model
+    in
+    (* Rename the probabilities to follow the convention in the paper.
+      Still uncertain if it actually makes it easier to understand transition
+      probability calculations. *)
+    let alpha = mp.gap_open in
+    let beta  = mp.gap_extension in
+    let gamma = mp.ending in
+    let tm = Array.make_matrix ~dimx:4 ~dimy:4 0. in
+    let ll = float ref_length in
+    let open Float in
+    tm.(match_idx).(match_idx)            <- (1. - 2. * alpha) * (1. - gamma);
+    tm.(match_idx).(insertion_idx)        <- alpha * (1. - gamma);
+    tm.(match_idx).(deletion_idx)         <- alpha * (1. - gamma);
+    tm.(match_idx).(start_end_idx)        <- gamma;
 
-  tm.(start_end_idx).(match_idx)        <- (1. - alpha) / ll;
-  tm.(start_end_idx).(insertion_idx)    <- alpha / ll;
-  (*tm.(start_end_idx).(deletion_idx)   <- 0.; *)
-  (*tm.(start_end_idx).(start_end_idx)  <- 0.; *)
-  tm
+    tm.(insertion_idx).(match_idx)        <- (1. - beta) * (1. - gamma);
+    tm.(insertion_idx).(insertion_idx)    <- beta * (1. - gamma);
+    (*tm.(insertion_idx).(deletion_idx)   <- 0.; *)
+    tm.(insertion_idx).(start_end_idx)    <- gamma;
 
+    tm.(deletion_idx).(match_idx)         <- (1. - beta);
+    (*tm.(deletion_idx).(insertion_idx)   <- 0.; *)
+    tm.(deletion_idx).(deletion_idx)      <- beta;
+    (*tm.(deletion_idx).(end_idx)         <- 0.; *)
+
+    tm.(start_end_idx).(match_idx)        <- (1. - alpha) / ll;
+    tm.(start_end_idx).(insertion_idx)    <- alpha / ll;
+    (*tm.(start_end_idx).(deletion_idx)   <- 0.; *)
+    (*tm.(start_end_idx).(start_end_idx)  <- 0.; *)
+    let to_index = function
+      | `Match      -> match_idx
+      | `Insert     -> insertion_idx
+      | `Delete     -> deletion_idx
+      | `StartOrEnd -> start_end_idx
+    in
+    fun f t -> tm.(to_index f).(to_index t)
+
+  type state = [ `Match | `Insert | `Delete | `StartOrEnd ]
+  type t = state -> state -> float
+end (* TransitionMatrix *)
 
 type emission_prob = int -> int -> float
-type transition_matrix = float array array
 
-type recurrences =
-  { start   : emission_prob -> insert_prob:float -> transition_matrix -> int -> float * float * float
-  ; match_  : emission_prob -> transition_matrix -> float array array -> i:int -> int -> float
-  ; insert  : insert_prob:float -> transition_matrix -> float array array -> i:int -> int -> float
-  ; delete  : transition_matrix -> float array array -> i:int -> int -> float
-  ; end_    : transition_matrix -> float array array -> i:int -> int -> float * float * float
+type fwd_recurrences =
+  { start   : emission_prob -> insert_prob:float -> TransitionMatrix.t -> int -> float * float * float
+  ; match_  : emission_prob -> TransitionMatrix.t -> float array array -> i:int -> int -> float
+  ; insert  : insert_prob:float -> TransitionMatrix.t -> float array array -> i:int -> int -> float
+  ; delete  : TransitionMatrix.t -> float array array -> i:int -> int -> float
+  ; end_    : TransitionMatrix.t -> float array array -> i:int -> int -> float * float * float
   }
 
 (* We are storing match/insert/delete states as sequential rows in the same
@@ -82,33 +92,33 @@ let di i = 3 * i + 2
 
 let forward_recurrences =
   { start   = begin fun emission_prob ~insert_prob tm k ->
-                tm.(start_end_idx).(match_idx) *. (emission_prob 0 k)
-                , tm.(start_end_idx).(insertion_idx) *. insert_prob
+                (tm `StartOrEnd `Match) *. (emission_prob 0 k)
+                , (tm `StartOrEnd `Insert) *. insert_prob
                 , 0.
               end
   ; match_  = begin fun emission_prob tm dm ~i -> function
                 | 0 -> 0.
                 | k -> (emission_prob i k) *.
-                          (  tm.(match_idx).(match_idx)      *. dm.(i-1).(mi (k - 1))
-                          +. tm.(insertion_idx).(match_idx)  *. dm.(i-1).(ii (k - 1))
-                          +. tm.(deletion_idx).(match_idx)   *. dm.(i-1).(di (k - 1)))
+                          (  (tm `Match `Match)   *. dm.(i-1).(mi (k - 1))
+                          +. (tm `Insert `Match)  *. dm.(i-1).(ii (k - 1))
+                          +. (tm `Delete `Match)  *. dm.(i-1).(di (k - 1)))
               end
    (* Notice that we do not transition between successive inserts: we're just
       extending the current (same k) insert. *)
   ; insert  = begin fun ~insert_prob tm dm ~i k ->
                 insert_prob *.
-                  (   tm.(match_idx).(insertion_idx)      *. dm.(i-1).(mi k)
-                  +.  tm.(insertion_idx).(insertion_idx)  *. dm.(i-1).(ii k))
+                  (   (tm `Match `Insert)   *. dm.(i-1).(mi k)
+                  +.  (tm `Insert `Insert)  *. dm.(i-1).(ii k))
               end
   ; delete  = begin fun tm dm ~i -> function
                 | 0 -> 0.
-                | k -> ( tm.(match_idx).(deletion_idx)    *. dm.(i).(mi (k - 1))
-                       +.tm.(deletion_idx).(deletion_idx) *. dm.(i).(di (k - 1)))
+                | k -> ( (tm `Match `Delete)  *. dm.(i).(mi (k - 1))
+                       +.(tm `Delete `Delete) *. dm.(i).(di (k - 1)))
               end
   ; end_    = begin fun tm dm ~i k ->
-                tm.(match_idx).(start_end_idx) *. dm.(i).(mi k)
-                , tm.(insertion_idx).(start_end_idx) *. dm.(i).(ii k)
-                , 0. (* since tm.(deletion_idx).(start_end_idx) = 0. *)
+                (tm `Match `StartOrEnd) *. dm.(i).(mi k)
+                , (tm `Insert `StartOrEnd) *. dm.(i).(ii k)
+                , 0. (* (since (tm `Delete `StartOrEnd) = 0. *)
               end
   }
 
@@ -119,7 +129,7 @@ let forward_recurrences =
 let forward_gen recurrences ~normalize ?model_probs ~refs ~read read_probs =
   let read_length = String.length read in   (* l *)
   let ref_length = String.length refs in    (* L *)
-  let tm = to_transition_matrix ?model_probs read_length ref_length in
+  let tm = TransitionMatrix.init ?model_probs read_length ref_length in
   let insert_prob = 0.25 in (* There are 4 characters, assume equality. *)
   let p_c_m i k =
     if read.[i] = refs.[k] then
@@ -128,7 +138,7 @@ let forward_gen recurrences ~normalize ?model_probs ~refs ~read read_probs =
       read_probs.(i) /. 3.
   in
   let m = Array.make_matrix ~dimx:(read_length + 1) ~dimy:(ref_length * 3) 0. in
-  let over_reference ~init ~g row f =
+  let over_row ~init ~g row f =
     let rec loop s k =
       if k = ref_length then
         s
@@ -142,11 +152,11 @@ let forward_gen recurrences ~normalize ?model_probs ~refs ~read read_probs =
     in
     loop init 0
   in
-  let sum_over_reference = over_reference ~init:0. ~g:(fun s mv iv dv -> s +. mv +. iv +. dv) in
-  let iter_over_reference = over_reference ~init:() ~g:(fun u _ _ _ -> u) in
+  let sum_over = over_row ~init:0. ~g:(fun s mv iv dv -> s +. mv +. iv +. dv) in
+  let iter_over = over_row ~init:() ~g:(fun u _ _ _ -> u) in
   let normalize row by =
     if normalize then
-      iter_over_reference row
+      iter_over row
         (fun k -> m.(row).(mi k) /. by
                 , m.(row).(ii k) /. by
                 , m.(row).(di k) /. by)
@@ -154,16 +164,16 @@ let forward_gen recurrences ~normalize ?model_probs ~refs ~read read_probs =
       ()
   in
   let rl1 = read_length - 1 in
-  let s1 = sum_over_reference 0 (recurrences.start p_c_m ~insert_prob tm) in
+  let s1 = sum_over 0 (recurrences.start p_c_m ~insert_prob tm) in
   normalize 0 s1;
   let f_m = recurrences.match_ p_c_m tm m in
   let i_m = recurrences.insert ~insert_prob tm m in
   let d_m = recurrences.delete tm m in
   for i = 1 to rl1 do
-    let s = sum_over_reference i (fun k -> f_m ~i k, i_m ~i k, d_m ~i k) in
+    let s = sum_over i (fun k -> f_m ~i k, i_m ~i k, d_m ~i k) in
     normalize i s;
   done;
-  let fl = sum_over_reference read_length (recurrences.end_ tm m ~i:rl1) in
+  let fl = sum_over read_length (recurrences.end_ tm m ~i:rl1) in
   normalize read_length fl;
   m, fl
 
@@ -198,19 +208,19 @@ let viterbi_recurrences =
                 | 0 -> 0.
                 | k -> (emission_prob i k) *.
                           (max3
-                            (tm.(match_idx).(match_idx)      *. dm.(i-1).(mi (k - 1)))
-                            (tm.(insertion_idx).(match_idx)  *. dm.(i-1).(ii (k - 1)))
-                            (tm.(deletion_idx).(match_idx)   *. dm.(i-1).(di (k - 1))))
+                            ((tm `Match `Match)      *. dm.(i-1).(mi (k - 1)))
+                            ((tm `Insert `Match)  *. dm.(i-1).(ii (k - 1)))
+                            ((tm `Delete `Match)   *. dm.(i-1).(di (k - 1))))
               end
   ; insert  = begin fun ~insert_prob tm dm ~i k ->
                 insert_prob *.
-                  (max (tm.(match_idx).(insertion_idx)      *. dm.(i-1).(mi k))
-                       (tm.(insertion_idx).(insertion_idx)  *. dm.(i-1).(ii k)))
+                  (max ((tm `Match `Insert)      *. dm.(i-1).(mi k))
+                       ((tm `Insert `Insert)  *. dm.(i-1).(ii k)))
               end
   ; delete  = begin fun tm dm ~i -> function
                 | 0 -> 0.
-                | k -> max (tm.(match_idx).(deletion_idx)     *. dm.(i).(mi (k - 1)))
-                           (tm.(deletion_idx).(deletion_idx)  *. dm.(i).(di (k - 1)))
+                | k -> max ((tm `Match `Delete)     *. dm.(i).(mi (k - 1)))
+                           ((tm `Delete `Delete)  *. dm.(i).(di (k - 1)))
               end
   }
 
@@ -299,3 +309,119 @@ let expand_path ?(insert_char ='-') ~refs ~read =
     | `D _ :: t     -> loop sref sread t
   in
   loop [] []
+
+type bwd_recurrences =
+  { last    : TransitionMatrix.t -> int -> float * float * float
+  ; first   : emission_prob -> insert_prob:float -> TransitionMatrix.t ->
+                float array array -> int -> float * float * float
+  ; match_  : emission_prob -> insert_prob:float -> TransitionMatrix.t ->
+                float array array -> i:int -> int -> float
+  ; insert  : emission_prob -> insert_prob:float -> TransitionMatrix.t ->
+                float array array -> i:int -> int -> float
+  ; delete  : emission_prob -> TransitionMatrix.t -> float array array ->
+                i:int -> int -> float
+  }
+
+let backward_recurrences ref_size =
+  { last    = begin fun tm _ ->
+                (tm `Match `StartOrEnd )
+                , (tm `Insert `StartOrEnd)
+                , 0.
+              end
+  ; first   = begin fun emission_prob ~insert_prob tm dm k ->
+                  (tm `StartOrEnd `Match) *. (emission_prob 0 k) *. dm.(1).(mi k)
+                , (tm `StartOrEnd `Insert) *. insert_prob *. dm.(1).(ii k)
+                , 0.
+              end
+  ; match_  = begin fun emission_prob ~insert_prob tm dm ~i k ->
+                let dmm, dmd =
+                  if k + 1 = ref_size then
+                    0., 0.
+                  else
+                    dm.(i+1).(mi (k+1)), dm.(i).(di (k+1))
+                in
+                let emission_p = emission_prob i (k + 1) in
+                   emission_p   *. (tm `Match `Match)   *. dmm
+                +. insert_prob  *. (tm `Match `Insert)  *. dm.(i+1).(ii k)
+                +.                 (tm `Match `Delete ) *. dmd
+              end
+  ; insert  = begin fun emission_prob ~insert_prob tm dm ~i k ->
+                let dmm =
+                  if k + 1 = ref_size then
+                    0.
+                  else
+                    dm.(i+1).(mi (k+1))
+                in
+                let emission_p = emission_prob i (k + 1) in
+                   emission_p   *. (tm `Insert `Match)  *. dmm
+                +. insert_prob  *. (tm `Insert `Insert) *. dm.(i+1).(ii k)
+              end
+  ; delete  = begin fun emission_prob tm dm ~i k->
+                let dmm, dmd =
+                    if k + 1 = ref_size then
+                      0., 0.
+                    else
+                      dm.(i+1).(mi (k+1)), dm.(i).(di (k+1))
+                in
+                let emission_p = emission_prob i (k + 1) in
+                emission_p *. (tm `Delete `Match)  *. dmm
+                +.            (tm `Delete `Delete) *. dmd
+              end
+ }
+
+let backward_gen recurrences ~normalize ?model_probs ~refs ~read read_probs =
+  let read_length = String.length read in   (* l *)
+  let ref_length = String.length refs in    (* L *)
+  let tm = TransitionMatrix.init ?model_probs read_length ref_length in
+  let insert_prob = 0.25 in
+  let p_c_m i k =
+    if k = ref_length || i = ref_length then
+      0.0
+    else if read.[i] = refs.[k] then
+      1. -. read_probs.(i)
+    else
+      read_probs.(i) /. 3.
+  in
+  let m = Array.make_matrix ~dimx:(read_length + 1) ~dimy:(ref_length * 3) 1. in
+  let over_row ~init ~g row f =
+    let rec loop s k =
+      if k < 0 then
+        s
+      else begin
+        let mv, iv, dv = f k in
+        m.(row).(mi k) <- mv;
+        m.(row).(ii k) <- iv;
+        m.(row).(di k) <- dv;
+        loop (g s mv iv dv) (k - 1)
+      end
+    in
+    loop init (ref_length - 1)
+  in
+  let sum_over = over_row ~init:0. ~g:(fun s mv iv dv -> s +. mv +. iv +. dv) in
+  let iter_over = over_row ~init:() ~g:(fun u _ _ _ -> u) in
+  let normalize row by =
+    if normalize then
+      iter_over row
+        (fun k -> m.(row).(mi k) /. by
+                , m.(row).(ii k) /. by
+                , m.(row).(di k) /. by)
+    else
+      ()
+  in
+  let rl1 = read_length - 1 in
+  let recurrences = recurrences ref_length in
+  let s1 = sum_over read_length (recurrences.last tm) in
+  normalize read_length s1;
+  let f_m = recurrences.match_ p_c_m ~insert_prob tm m in
+  let i_m = recurrences.insert p_c_m ~insert_prob tm m in
+  let d_m = recurrences.delete p_c_m tm m in
+  for i = rl1 downto 1 do
+    let s = sum_over i (fun k -> f_m ~i k, i_m ~i k, d_m ~i k) in
+    normalize i s;
+  done;
+  let fl = sum_over 0 (recurrences.first p_c_m ~insert_prob tm m) in
+  normalize 0 fl;
+  m, fl
+
+let backward ?(normalize=true) =
+  backward_gen ~normalize backward_recurrences
