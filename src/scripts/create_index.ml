db25f494dfc7cdc46ab9f32d4703882f0346285c
Leonid Rozenberg <leonidr@gmail.com>
2017-04-06 13:02:09 -0400

    Update to new format

--- src/scripts/create_index.ml
+++ src/scripts/create_index.ml
@@ -4,21 +4,22 @@ open Common
 let k = 10 ;;
 
 let default_input =
-  Alleles.Input.AlignmentFile (to_alignment_file "A_gen", false) ;;
+  Alleles.Input.AlignmentFile (to_alignment_file "A_gen", true) ;;
 
 let gall, idxall =
-  Cache.(graph_and_two_index { k = k ; graph_args = graph_args ~input:default_input () }) ;;
+  Cache.(graph_and_two_index { k = k
+        ; graph_args = graph_args ~input:default_input
+                          ~arg:Ref_graph.default_construction_arg  }) ;;
 
-let g_and_idx ?(k=10) ?input ?n () =
+let g_and_idx ?(k=10) ?input ~arg () =
   let default = Alleles.Input.AlignmentFile (to_alignment_file "A_nuc", false) in
   let input = Option.value input ~default in
-  let g2 =
-    Cache.(graph_and_two_index { k = k; graph_args = graph_args ?n ~input () })
-  in
-  n, g2
+  Cache.(graph_and_two_index { k = k; graph_args = graph_args ~input ~arg })
 
+  (*
 let _, (gm, im) =
   g_and_idx ~input:(Alleles.Input.MergeFromPrefix
                       (to_merge_prefix "C"
                       , Distances.AverageExon
                       , false)) ()
+                      *)
