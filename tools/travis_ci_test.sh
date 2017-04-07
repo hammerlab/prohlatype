43fe0f9ce73a9f96dad08cfbc08f88feb048c314
Leonid Rozenberg <leonidr@gmail.com>
2017-04-07 10:49:57 -0400

    Correct exec name in travis build env

--- tools/travis_ci_test.sh
+++ tools/travis_ci_test.sh
@@ -71,11 +71,11 @@ case "$TEST" in
     ;;
   impute)
     echo imputing A
-    time ./mhc2gpdf $IMGTHLA_DIR/alignments/A_gen.txt --no-pdf
+    time ./mhc2gpdf.native -f $IMGTHLA_DIR/alignments/A_gen.txt --no-pdf
     echo imputing B
-    time ./mhc2gpdf $IMGTHLA_DIR/alignments/B_gen.txt --no-pdf
+    time ./mhc2gpdf.native -f $IMGTHLA_DIR/alignments/B_gen.txt --no-pdf
     echo imputing C
-    time ./mhc2gpdf $IMGTHLA_DIR/alignments/C_gen.txt --no-pdf
+    time ./mhc2gpdf.native -f $IMGTHLA_DIR/alignments/C_gen.txt --no-pdf
     ;;
   *)
     ;;
