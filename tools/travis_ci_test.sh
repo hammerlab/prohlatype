e3ba3b830b60f072fa977588bde06590c87b4f21
Leonid Rozenberg <leonidr@gmail.com>
2017-04-17 11:17:30 -0400

    Fix travis to export only covered tests.

--- tools/travis_ci_test.sh
+++ tools/travis_ci_test.sh
@@ -26,28 +26,34 @@ case "$TEST" in
   parsing)
     echo testing parsing
     ./test_parsing.native
+    export BISECT="true"
     ;;
   mas)
     echo mas alignment
     cp src/scripts/mas_align_script.sh .
     ./mas_align_script.sh
+    export BISECT="true"
     ;;
   round)
     echo testing round trip graph construction
     cp src/scripts/round_trip_tests.sh .
     ./round_trip_tests.sh
+    export BISECT="true"
     ;;
   mergeA)
     echo testing merging of A
     ./merged_sensible_test.native A
+    export BISECT="true"
     ;;
   mergeB)
     echo testing merging of B
     ./merged_sensible_test.native B
+    export BISECT="true"
     ;;
   mergeC)
     echo testing merging of C
     ./merged_sensible_test.native C
+    export BISECT="true"
     ;;
   adj)
     echo testing adjcent finding for A_gen
@@ -58,15 +64,18 @@ case "$TEST" in
     time ./adjacents.native B_gen 2>/dev/null
     echo testing adjcent finding for B_nuc
     time ./adjacents.native B_nuc 2>/dev/null
+    export BISECT="true"
     ;;
   alleleDiffA)
     echo testing allele differences between A_gen
     time ./test_allele_distances.native A_gen > A_sim.csv
+    export BISECT="true"
     ;;
   biologicalKmers)
     echo testing that biological Kmers are found
     cp src/scripts/biological_kmer_tests.sh .
     time ./biological_kmer_tests.sh
+    export BISECT="true"
     ;;
   impute)
     echo imputing A
@@ -87,6 +96,6 @@ esac
 #cp src/scripts/adjacent_tests.sh .
 #./adjacent_tests.sh
 
-if [ -n "$TEST" ] ; then
+if [ -n "$TEST" -a -n "$BISECT" ] ; then
   ocveralls --repo_token $COVERALLSTOKEN --git --send bisect*.out
 fi
