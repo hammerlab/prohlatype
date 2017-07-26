dc9fab510e289aedd32bc7fa8e95c959f370c18f
Leonid Rozenberg <leonidr@gmail.com>
2017-07-26 12:08:04 -0400

    Separate useful function.

--- src/scripts/just_parse.ml
+++ src/scripts/just_parse.ml
@@ -1,11 +1,14 @@
 (* Old, deprecated, test of the parsing logic. *)
-let f = "A_nuc.txt" ;;
+let f = "A_gen.txt" ;;
 let d = "../foreign/IMGTHLA/alignments" ;;
 let mp = MSA.Parser.from_file (Filename.concat d f) ;;
 let just_gaps = (function MSA.Gap _ -> true | _ -> false) ;;
 let just_bns = (function MSA.Boundary _ -> true | _ -> false) ;;
+
+let print_alignment_sequence l =
+    List.map l ~f:MSA.al_el_to_string
+    |> List.iter ~f:(Printf.printf "%s\n") ;;
+
 let print_parsed oa =
   List.Assoc.get oa mp.MSA.Parser.alt_elems
-  |> Option.map ~f:(fun l ->
-    List.map l ~f:MSA.al_el_to_string
-    |> List.iter ~f:(Printf.printf "%s\n")) ;;
+  |> Option.map ~f:print_alignment_sequence ;;
