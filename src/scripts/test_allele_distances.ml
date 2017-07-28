2d1bb82fec1158722559f4574e7d1a8a0c7bebc8
Leonid Rozenberg <leonidr@gmail.com>
2017-07-27 21:54:25 -0400

    Split up the debugging messages

--- src/scripts/test_allele_distances.ml
+++ src/scripts/test_allele_distances.ml
@@ -10,17 +10,19 @@ let () =
     let file = if n < 2 then "A_gen" else Sys.argv.(1) in
     let mp = from_file (Common.to_alignment_file file) in
     List.iter mp.alt_elems ~f:(fun (al1, allele) ->
+      printf "ref vs %s:%!" al1;
       let ds = 
         Segments.distances ~reference:mp.ref_elems ~allele
         |> List.map ~f:(fun d -> sprintf "%d" d.Segments.mismatches)
         |> String.concat ~sep:";"
       in
-      printf "ref vs %s: %s\n" al1 ds;
+      printf "%s\n" ds;
       List.iter mp.alt_elems ~f:(fun (al2, allele2) ->
+        printf " %s vs %s:" al1 al2;
         let dvs =
           Segments.distances_between ~reference:mp.ref_elems
             ~allele1:allele ~allele2
           |> List.map ~f:(fun d -> sprintf "%d" d.Segments.mismatches)
           |> String.concat ~sep:";"
         in
-        printf " %s vs %s: %s\n" al1 al2 dvs))
+        printf " %s\n" dvs))
