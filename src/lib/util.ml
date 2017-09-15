a7fd4979f5f378a99294855a665a9c7d2812fc00
Leonid Rozenberg <leonidr@gmail.com>
2017-09-15 14:28:49 -0400

    Add a GC comparison function

--- src/lib/util.ml
+++ src/lib/util.ml
@@ -251,6 +251,48 @@ let time s f =
   printf "%s total running time in seconds: %f\n%!" s (Sys.time () -. n);
   r
 
+let gc_between s f =
+  let open Gc in
+  let before = stat () in
+  let r = f () in
+  let after = stat () in
+  printf "%s Gc change: \n\
+    \t { minor_words : %f;\n\
+    \t   promoted_words : %f;\n\
+    \t   major_words : %f;\n\
+    \t   minor_collections : %d;\n\
+    \t   major_collections : %d;\n\
+    \t   heap_words : %d;\n\
+    \t   heap_chunks : %d;\n\
+    \t   live_words : %d;\n\
+    \t   live_blocks : %d;\n\
+    \t   free_words : %d;\n\
+    \t   free_blocks : %d;\n\
+    \t   largest_free : %d;\n\
+    \t   fragments : %d;\n\
+    \t   compactions : %d;\n\
+    \t   top_heap_words : %d;\n\
+    \t   stack_size : %d;\n\
+    \t }\n"
+      s
+      (after.minor_words -. before.minor_words)
+      (after.promoted_words -. before.promoted_words)
+      (after.major_words -. before.major_words)
+      (after.minor_collections - before.minor_collections)
+      (after.major_collections - before.major_collections)
+      (after.heap_words - before.heap_words)
+      (after.heap_chunks - before.heap_chunks)
+      (after.live_words - before.live_words)
+      (after.live_blocks - before.live_blocks)
+      (after.free_words - before.free_words)
+      (after.free_blocks - before.free_blocks)
+      (after.largest_free - before.largest_free)
+      (after.fragments - before.fragments)
+      (after.compactions - before.compactions)
+      (after.top_heap_words - before.top_heap_words)
+      (after.stack_size - before.stack_size);
+  r
+
 type 'a single_or_paired =
   | Single of 'a
   | Paired of ('a * 'a)
