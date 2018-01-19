f5c4b88b1ebfefda38fefd593b352f311ba21124
Leonid Rozenberg <leonidr@gmail.com>
2018-01-19 14:57:32 -0500

    Remove useless 'test' from testing scripts

--- tools/travis_ci_test.sh
+++ tools/travis_ci_test.sh
@@ -29,44 +29,44 @@ case "$TEST" in
   parsing)
     make covered_tests
     echo testing parsing
-    _build/default/src/tests/test_parsing.exe
+    _build/default/src/tests/alignment_parsing.exe
     export BISECT="true"
     ;;
   mas)
     echo mas alignment
     make covered_tests
     cp src/tests/mas_align_script.sh .
-    cp _build/default/src/tests/mas_align_test.exe .
+    cp _build/default/src/tests/mas_align.exe .
     ./mas_align_script.sh
     export BISECT="true"
     ;;
   round)
     echo testing round trip graph construction
     make covered_tests
-    cp src/tests/round_trip_tests.sh .
+    cp src/tests/round_trips.sh .
     cp _build/default/src/tests/round_trip.exe .
-    ./round_trip_tests.sh
+    ./round_trips.sh
     export BISECT="true"
     ;;
   mergeA)
     echo testing merging of A
     make tests
-    time _build/default/src/tests/merged_sensible_test.exe A > merged_A.log
+    time _build/default/src/tests/merged_sensible.exe A > merged_A.log
     ;;
   mergeB)
     echo testing merging of B
     make tests
-    time _build/default/src/tests/merged_sensible_test.exe B > merged_B.log
+    time _build/default/src/tests/merged_sensible.exe B > merged_B.log
     ;;
   mergeC)
     echo testing merging of C
     make tests
-    time _build/default/src/tests/merged_sensible_test.exe C > merged_C.log
+    time _build/default/src/tests/merged_sensible.exe C > merged_C.log
     ;;
  alleleDiffA)
     echo testing allele differences between A_gen
     make covered_tests
-    time _build/default/src/tests/test_allele_distances.exe A_gen > A_sim.csv
+    time _build/default/src/tests/allele_distances.exe A_gen > A_sim.csv
     export BISECT="true"
     ;;
   impute)
